66e69dcf8b0377914155f6f36c0ec6b1b3606de3
Anik Sen <cse2023anik@gmail.com>
2024-04-03 04:46:37 +0600

    Update Small_Factorial.cpp

--- Small_Factorial.cpp
+++ Small_Factorial.cpp
@@ -1,15 +1,23 @@
-#include<iostream>
+#include <bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
 using namespace std;
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int facto=1;
-        for(int i=1; i<=n; i++){
-            facto*=i;
-        }
-        cout<<facto<<endl;
-    }return 0;
+using namespace boost::multiprecision;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    cpp_int prod=1;
+	    for(int i=n;i>0;i--){
+	        prod=prod*i;
+	    }
+	     
+	    cout<<prod<<endl; 
+	}
+	
+	return 0;
 }
